9a74241bff89936dcc6012a91b5f7c73fbf10cda
k-szczeciak <szczeciak@hotmail.com>
2019-01-28 22:42:14 +0100

    zadanie 1

--- src/main/java/pl/coderslab/Servlet01.java
+++ src/main/java/pl/coderslab/Servlet01.java
@@ -10,11 +10,80 @@ import java.io.IOException;
 @WebServlet("/Servlet01")
 public class Servlet01 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=utf-8");
+        String amount = request.getParameter("amount");
+        double amountDouble = 0;
+        double eur_usd = 1.1427;
+        double usd_eur = 1/eur_usd;
+        double eur_pln = 4.2885;
+        double pln_eur = 1/eur_pln;
+        double usd_pln = 3.7592;
+        double pln_usd = 1/usd_pln;
+        String curPair = request.getParameter("curPair");
+
+        try{
+            amountDouble = Double.parseDouble(amount);
+        }catch (NumberFormatException e){
+            response.getWriter().append("niewaciwa wartość").append("<br>");
+        }
+        if (curPair == null){
+            response.getWriter().append("brak wyboru pary walut").append("<br>");
+        }else{
+            switch (curPair){
+                case "EUR_USD":{
+                    double result = amountDouble * eur_usd;
+                    response.getWriter().append("Stan na dzień 2019.01.28:").append("<br>");
+                    response.getWriter().append(String.format( "%.2f", result )).append(" USD").append("<br>");
+                    break;
+                }
+                case "USD_EUR":{
+                    double result = amountDouble * usd_eur;
+                    response.getWriter().append("Stan na dzień 2019.01.28:").append("<br>");
+                    response.getWriter().append(String.format( "%.2f", result )).append(" EUR").append("<br>");
+                    break;
+                }
+                case "EUR_PLN":{
+                    double result = amountDouble * eur_pln;
+                    response.getWriter().append("Stan na dzień 2019.01.28:").append("<br>");
+                    response.getWriter().append(String.format( "%.2f", result )).append(" PLN").append("<br>");
+                    break;
+                }
+                case "PLN_EUR":{
+                    double result = amountDouble * pln_eur;
+                    response.getWriter().append("Stan na dzień 2019.01.28:").append("<br>");
+                    response.getWriter().append(String.format( "%.2f", result )).append(" EUR").append("<br>");
+                    break;
+                }
+                case "USD_PLN":{
+                    double result = amountDouble * usd_pln;
+                    response.getWriter().append("Stan na dzień 2019.01.28:").append("<br>");
+                    response.getWriter().append(String.format( "%.2f", result )).append(" PLN").append("<br>");
+                    break;
+                }
+                case "PLN_USD":{
+                    double result = amountDouble * pln_usd;
+                    response.getWriter().append("Stan na dzień 2019.01.28:").append("<br>");
+                    response.getWriter().append(String.format( "%.2f", result )).append(" USD").append("<br>");
+                    break;
+                }
+                default:response.getWriter().append("niewaciwa wartość");
+            }
+        }
+        //response.getWriter().append(String.valueOf(result)).append("<br>");
+
+
+
+        //response.getWriter().append("amount: ").append(amount).append(", ").append(curPair);
+
+
+
+
+
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=utf-8");
-        response.getWriter().append("test:");
+
     }
 }
